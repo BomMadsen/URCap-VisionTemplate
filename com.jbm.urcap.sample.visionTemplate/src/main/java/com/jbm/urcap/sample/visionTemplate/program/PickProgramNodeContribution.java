686fd7dc0e284961259fe53993ebdc2f1e06b059
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2019-12-16 20:11:16 +0100

    Moved creation of PartsFound template from VisionPick into PartsFound node for readability

--- com.jbm.urcap.sample.visionTemplate/src/main/java/com/jbm/urcap/sample/visionTemplate/program/PickProgramNodeContribution.java
+++ com.jbm.urcap.sample.visionTemplate/src/main/java/com/jbm/urcap/sample/visionTemplate/program/PickProgramNodeContribution.java
@@ -1,7 +1,5 @@
 package com.jbm.urcap.sample.visionTemplate.program;
 
-import java.util.List;
-
 import com.jbm.urcap.sample.visionTemplate.templateNodes.ErrorProgramService;
 import com.jbm.urcap.sample.visionTemplate.templateNodes.NoAvailablePartsProgramService;
 import com.jbm.urcap.sample.visionTemplate.templateNodes.PartsFoundAndReachableProgramService;
@@ -10,38 +8,13 @@ import com.ur.urcap.api.contribution.program.CreationContext.NodeCreationType;
 import com.ur.urcap.api.contribution.program.CreationContext;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.domain.data.DataModel;
-import com.ur.urcap.api.domain.device.gripper.GripperDevice;
-import com.ur.urcap.api.domain.device.gripper.GripperManager;
 import com.ur.urcap.api.domain.program.ProgramModel;
-import com.ur.urcap.api.domain.program.nodes.ProgramNode;
 import com.ur.urcap.api.domain.program.nodes.ProgramNodeFactory;
-import com.ur.urcap.api.domain.program.nodes.builtin.CommentNode;
-import com.ur.urcap.api.domain.program.nodes.builtin.FolderNode;
-import com.ur.urcap.api.domain.program.nodes.builtin.MoveNode;
-import com.ur.urcap.api.domain.program.nodes.builtin.WaypointNode;
-import com.ur.urcap.api.domain.program.nodes.builtin.configurations.movenode.MoveJMoveNodeConfig;
-import com.ur.urcap.api.domain.program.nodes.builtin.configurations.movenode.MoveLMoveNodeConfig;
-import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.BlendParameters;
-import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.WaypointMotionParameters;
-import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.WaypointNodeConfig;
-import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.GripperNode;
-import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.GripperNodeFactory;
-import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.configuration.GripActionConfig;
 import com.ur.urcap.api.domain.program.structure.TreeNode;
 import com.ur.urcap.api.domain.program.structure.TreeStructureException;
 import com.ur.urcap.api.domain.script.ScriptWriter;
 import com.ur.urcap.api.domain.undoredo.UndoRedoManager;
 import com.ur.urcap.api.domain.undoredo.UndoableChanges;
-import com.ur.urcap.api.domain.value.Pose;
-import com.ur.urcap.api.domain.value.PoseFactory;
-import com.ur.urcap.api.domain.value.expression.ExpressionBuilder;
-import com.ur.urcap.api.domain.value.expression.InvalidExpressionException;
-import com.ur.urcap.api.domain.value.simple.Angle;
-import com.ur.urcap.api.domain.value.simple.Length;
-import com.ur.urcap.api.domain.variable.GlobalVariable;
-import com.ur.urcap.api.domain.variable.Variable;
-import com.ur.urcap.api.domain.variable.VariableException;
-import com.ur.urcap.api.domain.variable.VariableFactory;
 
 public class PickProgramNodeContribution implements ProgramNodeContribution{
 
@@ -49,10 +22,6 @@ public class PickProgramNodeContribution implements ProgramNodeContribution{
 	private final PickProgramNodeView view;
 	private final DataModel model;
 	private final UndoRedoManager undoRedoManager;
-	
-	private static final String VARIABLE_APPROACH_KEY = "approachWpt";
-	private static final String VARIABLE_TARGET_KEY = "targetWpt";
-	private static final String VARIABLE_EXIT_KEY = "exitWpt";
 	
 	public PickProgramNodeContribution(ProgramAPIProvider apiProvider, PickProgramNodeView view,
 			DataModel model, CreationContext context) {
@@ -74,11 +43,7 @@ public class PickProgramNodeContribution implements ProgramNodeContribution{
 			@Override
 			public void executeChanges() {
 				try {
-					/*****
-					 * This section is getting looooong, maybe split me up!
-					 */
-					
-					// Build basic template
+					// Insert child template nodes
 					root.addChild(nf.createURCapProgramNode(PartsFoundAndReachableProgramService.class));
 					root.addChild(nf.createURCapProgramNode(NoAvailablePartsProgramService.class));
 					root.addChild(nf.createURCapProgramNode(ErrorProgramService.class));
@@ -86,130 +51,11 @@ public class PickProgramNodeContribution implements ProgramNodeContribution{
 					// Lock the sequence of branches
 					root.setChildSequenceLocked(true);
 					
-					// Build template for PartsFoundNode
-					TreeNode partsFoundTreeNode = root.getChildren().get(0);
-					
-					// Build a MoveJ for approach
-					MoveNode approachMoveJ = nf.createMoveNodeNoTemplate();
-					approachMoveJ.setConfig(approachMoveJ.getConfigBuilders().createMoveJConfigBuilder().build());
-					
-					// Build approach waypoint
-					WaypointNode approachWaypoint = nf.createWaypointNode();
-					GlobalVariable approachVariable = createPoseVariable("Approach");
-					storeApproachVariable(approachVariable); //Store the variable in the data model for later usage
-					BlendParameters approachWaypointDefaultBlends = approachWaypoint.getConfigFactory().createNoBlendParameters();
-					WaypointMotionParameters approachMotionParams = approachWaypoint.getConfigFactory().createSharedMotionParameters();
-					WaypointNodeConfig approachConfig = approachWaypoint.getConfigFactory().createVariablePositionConfig(approachVariable, approachWaypointDefaultBlends, approachMotionParams);
-					approachWaypoint.setConfig(approachConfig);
-					
-					// Build a MoveL for target
-					MoveNode targetMoveL = nf.createMoveNodeNoTemplate();
-					targetMoveL.setConfig(targetMoveL.getConfigBuilders().createMoveLConfigBuilder().build());
-					
-					// Build target waypoint
-					WaypointNode targetWaypoint = nf.createWaypointNode();
-					GlobalVariable targetVariable = createPoseVariable("Target");
-					storeTargetVariable(targetVariable);
-					BlendParameters targetWaypointDefaultBlends = targetWaypoint.getConfigFactory().createNoBlendParameters();
-					WaypointMotionParameters targetMotionParams = targetWaypoint.getConfigFactory().createSharedMotionParameters();
-					WaypointNodeConfig targetConfig = targetWaypoint.getConfigFactory().createVariablePositionConfig(targetVariable, targetWaypointDefaultBlends, targetMotionParams);
-					targetWaypoint.setConfig(targetConfig);
-					
-					// Build a MoveL for exit
-					MoveNode exitMoveL = nf.createMoveNodeNoTemplate();
-					exitMoveL.setConfig(exitMoveL.getConfigBuilders().createMoveJConfigBuilder().build());
-					
-					// Build exit waypoint
-					WaypointNode exitWaypoint = nf.createWaypointNode();
-					GlobalVariable exitVariable = createPoseVariable("Exit");
-					storeExitVariable(exitVariable);
-					BlendParameters exitWaypointDefaultBlends = exitWaypoint.getConfigFactory().createNoBlendParameters();
-					WaypointMotionParameters exitMotionParams = exitWaypoint.getConfigFactory().createSharedMotionParameters();
-					WaypointNodeConfig exitConfig = exitWaypoint.getConfigFactory().createVariablePositionConfig(exitVariable, exitWaypointDefaultBlends, exitMotionParams);
-					exitWaypoint.setConfig(exitConfig);
-					
-					// Now we have build all the components. 
-					// Let's put them into a tree
-					TreeNode approachMoveTreeNode = partsFoundTreeNode.addChild(approachMoveJ);
-					approachMoveTreeNode.addChild(approachWaypoint);
-					
-					TreeNode targetMoveTreeNode = partsFoundTreeNode.addChild(targetMoveL);
-					targetMoveTreeNode.addChild(targetWaypoint);
-					
-					partsFoundTreeNode.addChild(createGripperNode());
-					
-					TreeNode exitMoveTreeNode = partsFoundTreeNode.addChild(exitMoveL);
-					exitMoveTreeNode.addChild(exitWaypoint);
-					
-					
 				} catch (TreeStructureException e) {
 					// TODO: handle exception
 				}
 			}
 		});
-	}
-	
-	private GlobalVariable createPoseVariable(String suggestedName) {
-		VariableFactory vf = apiProvider.getProgramAPI().getVariableModel().getVariableFactory();
-		PoseFactory pf = apiProvider.getProgramAPI().getValueFactoryProvider().getPoseFactory();
-		ExpressionBuilder eb = apiProvider.getProgramAPI().getValueFactoryProvider().createExpressionBuilder();
-		
-		// Create an empty pose, will be updated by program later
-		Pose initialPose = pf.createPose(0, 0, 0, 0, 0, 0, Length.Unit.M, Angle.Unit.RAD);
-		
-		GlobalVariable var = null;
-		try {
-			var = vf.createGlobalVariable(suggestedName, eb.append(initialPose.toString()).build());
-		} catch (VariableException e) {
-			System.out.println("Exception creating pose variable...");
-		} catch (InvalidExpressionException e) {
-			System.out.println("Exception building pose...");
-		}
-		return var;
-	}
-	
-	private ProgramNode createGripperNode() {
-		GripperNodeFactory gnf = apiProvider.getProgramAPI().getDeviceManager(GripperManager.class).getGripperProgramNodeFactory();
-		ProgramNodeFactory pnf = apiProvider.getProgramAPI().getProgramModel().getProgramNodeFactory();
-		
-		int gripperCount = getGripperCount();
-		if (gripperCount==1) {
-			// Only one gripper driver available, easy to guess which to choose
-			
-			GripperDevice gripper = getGrippers().get(0);
-			GripperNode gripperNode = gnf.createGripperNode(gripper);
-			
-			GripActionConfig gripConfig = gripperNode.createGripActionConfigBuilder().build();
-			gripperNode.setConfig(gripConfig);
-			
-			return gripperNode;
-		} else if (gripperCount > 1) {
-			// More than one gripper available
-			
-			// TODO Handle letting the user select which gripper to use
-			GripperDevice gripper = getGrippers().get(1);
-			GripperNode gripperNode = gnf.createGripperNode(gripper);
-			
-			GripActionConfig gripConfig = gripperNode.createGripActionConfigBuilder().build();
-			gripperNode.setConfig(gripConfig);
-			
-			return gripperNode;
-		} else {
-			// No gripper drivers available, insert a folder instead
-			
-			FolderNode gripperFolder = pnf.createFolderNode();
-			gripperFolder.setName("Gripper Grip");
-			
-			return gripperFolder;
-		}
-	}
-	
-	private List<GripperDevice> getGrippers(){
-		return apiProvider.getProgramAPI().getDeviceManager(GripperManager.class).getGrippers();
-	}
-	
-	private int getGripperCount() {
-		return getGrippers().size();
 	}
 	
 	@Override
@@ -237,31 +83,15 @@ public class PickProgramNodeContribution implements ProgramNodeContribution{
 	@Override
 	public void generateScript(ScriptWriter writer) {
 		
-		// We need to oay attention to, that the variable name may have changed
-		// In order to get the variable for the program, this must be done: 
-		String actualApproachVariableName = writer.getResolvedVariableName(getApproachWaypointVariable());
-		// Or we can just use this in an assignment:
-		writer.assign(getApproachWaypointVariable(), "p[0.1, 0.2, 0.3, 0, 3.1415, 0]");
-		writer.appendLine("popup(\"Actual approach name is: "+actualApproachVariableName+"\",\"Vision Message\", blocking=True)");
+//		// We need to oay attention to, that the variable name may have changed
+//		// In order to get the variable for the program, this must be done: 
+//		String actualApproachVariableName = writer.getResolvedVariableName(getApproachWaypointVariable());
+//		// Or we can just use this in an assignment:
+//		writer.assign(getApproachWaypointVariable(), "p[0.1, 0.2, 0.3, 0, 3.1415, 0]");
+//		writer.appendLine("popup(\"Actual approach name is: "+actualApproachVariableName+"\",\"Vision Message\", blocking=True)");
 		
 		writer.writeChildren();
 	}
 	
-	private Variable getApproachWaypointVariable() {
-		return model.get(VARIABLE_APPROACH_KEY, createPoseVariable("Approach"));
-	}
-	
-	// NOTE: These are already called inside an "UndoableChanges" scope
-	private void storeApproachVariable(Variable var) {
-		model.set(VARIABLE_APPROACH_KEY, var);
-	}
-	
-	private void storeTargetVariable(Variable var) {
-		model.set(VARIABLE_TARGET_KEY, var);
-	}
-	
-	private void storeExitVariable(Variable var) {
-		model.set(VARIABLE_EXIT_KEY, var);
-	}
 
 }
--- com.jbm.urcap.sample.visionTemplate/src/main/java/com/jbm/urcap/sample/visionTemplate/templateNodes/PartsFoundAndReachableProgramContribution.java
+++ com.jbm.urcap.sample.visionTemplate/src/main/java/com/jbm/urcap/sample/visionTemplate/templateNodes/PartsFoundAndReachableProgramContribution.java
@@ -1,16 +1,206 @@
 package com.jbm.urcap.sample.visionTemplate.templateNodes;
 
+import java.util.List;
+
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.contribution.program.CreationContext;
+import com.ur.urcap.api.contribution.program.CreationContext.NodeCreationType;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.domain.data.DataModel;
+import com.ur.urcap.api.domain.device.gripper.GripperDevice;
+import com.ur.urcap.api.domain.device.gripper.GripperManager;
+import com.ur.urcap.api.domain.program.ProgramModel;
+import com.ur.urcap.api.domain.program.nodes.ProgramNode;
+import com.ur.urcap.api.domain.program.nodes.ProgramNodeFactory;
+import com.ur.urcap.api.domain.program.nodes.builtin.FolderNode;
+import com.ur.urcap.api.domain.program.nodes.builtin.MoveNode;
+import com.ur.urcap.api.domain.program.nodes.builtin.WaypointNode;
+import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.BlendParameters;
+import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.WaypointMotionParameters;
+import com.ur.urcap.api.domain.program.nodes.builtin.configurations.waypointnode.WaypointNodeConfig;
+import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.GripperNode;
+import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.GripperNodeFactory;
+import com.ur.urcap.api.domain.program.nodes.contributable.device.gripper.configuration.GripActionConfig;
+import com.ur.urcap.api.domain.program.structure.TreeNode;import com.ur.urcap.api.domain.program.structure.TreeStructureException;
 import com.ur.urcap.api.domain.script.ScriptWriter;
+import com.ur.urcap.api.domain.undoredo.UndoRedoManager;
+import com.ur.urcap.api.domain.undoredo.UndoableChanges;
+import com.ur.urcap.api.domain.value.Pose;
+import com.ur.urcap.api.domain.value.PoseFactory;
+import com.ur.urcap.api.domain.value.expression.ExpressionBuilder;
+import com.ur.urcap.api.domain.value.expression.InvalidExpressionException;
+import com.ur.urcap.api.domain.value.simple.Angle;
+import com.ur.urcap.api.domain.value.simple.Length;
+import com.ur.urcap.api.domain.variable.GlobalVariable;
+import com.ur.urcap.api.domain.variable.Variable;
+import com.ur.urcap.api.domain.variable.VariableException;
+import com.ur.urcap.api.domain.variable.VariableFactory;
 
 public class PartsFoundAndReachableProgramContribution implements ProgramNodeContribution{
 
+	private final ProgramAPIProvider apiProvider;
+	private final DataModel model;
+	private final PartsFoundAndReachableProgramView view;
+	private final UndoRedoManager undoRedoManager;
+	
+	private static final String VARIABLE_APPROACH_KEY = "approachWpt";
+	private static final String VARIABLE_TARGET_KEY = "targetWpt";
+	private static final String VARIABLE_EXIT_KEY = "exitWpt";
+	
 	public PartsFoundAndReachableProgramContribution(ProgramAPIProvider apiProvider,
 			PartsFoundAndReachableProgramView view, DataModel model, CreationContext context) {
-		// TODO Auto-generated constructor stub
+		this.apiProvider = apiProvider;
+		this.model = model; 
+		this.view = view;
+		this.undoRedoManager = apiProvider.getProgramAPI().getUndoRedoManager();
+		
+		if(context.getNodeCreationType().equals(NodeCreationType.NEW)) {
+			if(getGripperCount()<=1) {
+				// If there is either 0 or 1 Gripper Drivers available, we just build the tree
+				implementTemplateTree();
+			} else {
+				// If more than 1 gripper is available, the user has to decide
+				lockTreeWaitingForGripperSelection();
+			}
+		}
+	}
+	
+	private void implementTemplateTree() {
+		final ProgramModel programModel = apiProvider.getProgramAPI().getProgramModel();
+		final ProgramNodeFactory nf = programModel.getProgramNodeFactory();
+		final TreeNode root = programModel.getRootTreeNode(this);
+		undoRedoManager.recordChanges(new UndoableChanges() {
+			@Override
+			public void executeChanges() {
+				try {
+					// Unlock child sequence
+					root.setChildSequenceLocked(false);
+					
+					// Build a MoveJ for approach
+					MoveNode approachMoveJ = nf.createMoveNodeNoTemplate();
+					approachMoveJ.setConfig(approachMoveJ.getConfigBuilders().createMoveJConfigBuilder().build());
+					
+					// Build approach waypoint
+					WaypointNode approachWaypoint = nf.createWaypointNode();
+					GlobalVariable approachVariable = createPoseVariable("Approach");
+					storeApproachVariable(approachVariable); //Store the variable in the data model for later usage
+					BlendParameters approachWaypointDefaultBlends = approachWaypoint.getConfigFactory().createNoBlendParameters();
+					WaypointMotionParameters approachMotionParams = approachWaypoint.getConfigFactory().createSharedMotionParameters();
+					WaypointNodeConfig approachConfig = approachWaypoint.getConfigFactory().createVariablePositionConfig(approachVariable, approachWaypointDefaultBlends, approachMotionParams);
+					approachWaypoint.setConfig(approachConfig);
+					
+					// Build a MoveL for target
+					MoveNode targetMoveL = nf.createMoveNodeNoTemplate();
+					targetMoveL.setConfig(targetMoveL.getConfigBuilders().createMoveLConfigBuilder().build());
+					
+					// Build target waypoint
+					WaypointNode targetWaypoint = nf.createWaypointNode();
+					GlobalVariable targetVariable = createPoseVariable("Target");
+					storeTargetVariable(targetVariable);
+					BlendParameters targetWaypointDefaultBlends = targetWaypoint.getConfigFactory().createNoBlendParameters();
+					WaypointMotionParameters targetMotionParams = targetWaypoint.getConfigFactory().createSharedMotionParameters();
+					WaypointNodeConfig targetConfig = targetWaypoint.getConfigFactory().createVariablePositionConfig(targetVariable, targetWaypointDefaultBlends, targetMotionParams);
+					targetWaypoint.setConfig(targetConfig);
+					
+					// Build a MoveL for exit
+					MoveNode exitMoveL = nf.createMoveNodeNoTemplate();
+					exitMoveL.setConfig(exitMoveL.getConfigBuilders().createMoveJConfigBuilder().build());
+					
+					// Build exit waypoint
+					WaypointNode exitWaypoint = nf.createWaypointNode();
+					GlobalVariable exitVariable = createPoseVariable("Exit");
+					storeExitVariable(exitVariable);
+					BlendParameters exitWaypointDefaultBlends = exitWaypoint.getConfigFactory().createNoBlendParameters();
+					WaypointMotionParameters exitMotionParams = exitWaypoint.getConfigFactory().createSharedMotionParameters();
+					WaypointNodeConfig exitConfig = exitWaypoint.getConfigFactory().createVariablePositionConfig(exitVariable, exitWaypointDefaultBlends, exitMotionParams);
+					exitWaypoint.setConfig(exitConfig);
+					
+					// Now we have build all the components. 
+					// Let's put them into a tree
+					TreeNode approachMoveTreeNode = root.addChild(approachMoveJ);
+					approachMoveTreeNode.addChild(approachWaypoint);
+					
+					TreeNode targetMoveTreeNode = root.addChild(targetMoveL);
+					targetMoveTreeNode.addChild(targetWaypoint);
+					
+					root.addChild(createGripperNode());
+					
+					TreeNode exitMoveTreeNode = root.addChild(exitMoveL);
+					exitMoveTreeNode.addChild(exitWaypoint);
+				} catch (TreeStructureException e) {
+					// TODO: handle exception
+				}
+			}
+		});
+	}
+	
+	private void lockTreeWaitingForGripperSelection() {
+		final ProgramModel programModel = apiProvider.getProgramAPI().getProgramModel();
+		final TreeNode root = programModel.getRootTreeNode(this);
+		root.setChildSequenceLocked(true);
+	}
+	
+	private GlobalVariable createPoseVariable(String suggestedName) {
+		VariableFactory vf = apiProvider.getProgramAPI().getVariableModel().getVariableFactory();
+		PoseFactory pf = apiProvider.getProgramAPI().getValueFactoryProvider().getPoseFactory();
+		ExpressionBuilder eb = apiProvider.getProgramAPI().getValueFactoryProvider().createExpressionBuilder();
+		
+		// Create an empty pose, will be updated by program later
+		Pose initialPose = pf.createPose(0, 0, 0, 0, 0, 0, Length.Unit.M, Angle.Unit.RAD);
+		
+		GlobalVariable var = null;
+		try {
+			var = vf.createGlobalVariable(suggestedName, eb.append(initialPose.toString()).build());
+		} catch (VariableException e) {
+			System.out.println("Exception creating pose variable...");
+		} catch (InvalidExpressionException e) {
+			System.out.println("Exception building pose...");
+		}
+		return var;
+	}
+	
+	private ProgramNode createGripperNode() {
+		GripperNodeFactory gnf = apiProvider.getProgramAPI().getDeviceManager(GripperManager.class).getGripperProgramNodeFactory();
+		ProgramNodeFactory pnf = apiProvider.getProgramAPI().getProgramModel().getProgramNodeFactory();
+		
+		int gripperCount = getGripperCount();
+		if (gripperCount==1) {
+			// Only one gripper driver available, easy to guess which to choose
+			
+			GripperDevice gripper = getGrippers().get(0);
+			GripperNode gripperNode = gnf.createGripperNode(gripper);
+			
+			GripActionConfig gripConfig = gripperNode.createGripActionConfigBuilder().build();
+			gripperNode.setConfig(gripConfig);
+			
+			return gripperNode;
+		} else if (gripperCount > 1) {
+			// More than one gripper available
+			
+			// TODO Handle letting the user select which gripper to use
+			GripperDevice gripper = getGrippers().get(1);
+			GripperNode gripperNode = gnf.createGripperNode(gripper);
+			
+			GripActionConfig gripConfig = gripperNode.createGripActionConfigBuilder().build();
+			gripperNode.setConfig(gripConfig);
+			
+			return gripperNode;
+		} else {
+			// No gripper drivers available, insert a folder instead
+			
+			FolderNode gripperFolder = pnf.createFolderNode();
+			gripperFolder.setName("Gripper Grip");
+			
+			return gripperFolder;
+		}
+	}
+	
+	private List<GripperDevice> getGrippers(){
+		return apiProvider.getProgramAPI().getDeviceManager(GripperManager.class).getGrippers();
+	}
+	
+	private int getGripperCount() {
+		return getGrippers().size();
 	}
 	
 	@Override
@@ -38,6 +228,19 @@ public class PartsFoundAndReachableProgramContribution implements ProgramNodeCon
 	@Override
 	public void generateScript(ScriptWriter writer) {
 		writer.writeChildren();
+	}
+	
+	// NOTE: These are already called inside an "UndoableChanges" scope
+	private void storeApproachVariable(Variable var) {
+		model.set(VARIABLE_APPROACH_KEY, var);
+	}
+	
+	private void storeTargetVariable(Variable var) {
+		model.set(VARIABLE_TARGET_KEY, var);
+	}
+	
+	private void storeExitVariable(Variable var) {
+		model.set(VARIABLE_EXIT_KEY, var);
 	}
 
 }
